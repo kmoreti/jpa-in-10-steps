8ee699ef980318dddc46a7ad68c9e1dc62322bab
kmoreti <kleber.camargo@fatec.sp.gov.br>
2018-02-20 21:46:45 -0300

    User entity has been created.

---
+++ src/main/java/br/com/moreti/jpain10steps/entity/User.java
@@ -0,0 +1,49 @@
+package br.com.moreti.jpain10steps.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String role;
+
+    protected User(){
+
+    }
+
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("User{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", role='").append(role).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
